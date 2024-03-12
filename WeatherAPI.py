53c165ee4c63a837931750833c4f3ccf0d8b4e70
Layton Cripps <cripps.layton@yahoo.co.uk>
2024-03-12 17:11:03 +0000

    final version

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -7,25 +7,36 @@ import customtkinter
 resultList = []
 
 #creating a function to send get requests to the API and display results
-def getWeather(list):
+def getWeather(list, font):
 
     try:
-        resultList[0].destroy()
-        resultList.pop(0)
+        print (len(list))
+        for i in range(0, len(list)):
+            list[0].destroy()
+            list.pop(0)
     except:
         pass
         
     #get request
     CITY = inputEntry.get()
-    BASE_URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=imperial&APPID={API_Key}"
+    BASE_URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=metric&APPID={API_Key}"
     response = requests.get(BASE_URL).json()
 
     #display the response
     #print (response)
-    print(f"In {response['name']} there is {response['weather'][0]['main']}")
-    searchResult = customtkinter.CTkLabel(app, text = f"In {response['name']} there is {response['weather'][0]['main']}")
-    list.append(searchResult)
-    searchResult.pack()
+    print(response)
+
+    searchResultCity = customtkinter.CTkLabel(app, text = f"City: {response['name']}", font = font)
+    list.append(searchResultCity)
+
+    searchResultWeather = customtkinter.CTkLabel(app, text = f"Weather: {response['weather'][0]['main']}")
+    list.append(searchResultWeather)
+
+    searchResultTemperature = customtkinter.CTkLabel(app, text = f"Temperature: {response['main']['temp']}°C")
+    list.append(searchResultTemperature)
+
+    for label in list:
+        label.pack()
 
 #system settings
 customtkinter.set_appearance_mode("dark")
@@ -38,6 +49,7 @@ app.title("Weather App")
 
 #UI
 text_font2 = ("", 18)
+bigger_font = ("", 18)
 
 CitySearchLabel = customtkinter.CTkLabel(app, text="weather search by city:", font=text_font2)
 CitySearchLabel.pack(pady = "20")
@@ -46,7 +58,7 @@ City = tk.StringVar()
 inputEntry = customtkinter.CTkEntry(app, width = 400, height = 40, textvariable = City)
 inputEntry.pack()
 
-SearchButton = customtkinter.CTkButton(app, text = "Search!", command = lambda : getWeather(resultList))
+SearchButton = customtkinter.CTkButton(app, text = "Search!", command = lambda : getWeather(resultList, bigger_font))
 SearchButton.pack(pady = 20)
 
 app.mainloop()
